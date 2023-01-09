f6020e811839f401cac8af235a58d0e04baf9fa4
s1tora <samirkhodzha@mail.ru>
2023-01-09 19:41:22 +0600

    Калькулятор

---
+++ calc.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+const calc = document.querySelector('.calc');
+const result = document.querySelector('#result');
+
+calc.addEventListener('click', function(event) {
+    if(!event.target.classList.contains('calc_btn')) return;
+    const value = (event.target.innerText);
+
+    switch (value) {
+
+        case 'AC':
+            result.innerText = '';
+            break;
+        
+        case '=':
+            result.innerText = eval(result.innerText).toFixed(2);
+            break;
+
+      
+        default:
+            result.innerText += value;
+        
+    }
+    
+   
+    
+}) });
